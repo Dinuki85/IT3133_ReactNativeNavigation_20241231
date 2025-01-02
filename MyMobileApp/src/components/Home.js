e495d5218247966bb50da47cb59305b81cff9fb4
Dinuki Prarthana <139613301+Dinuki85@users.noreply.github.com>
2025-01-02 16:16:32 +0000

    feat:Add basic structure for Home component

--- MyMobileApp/src/components/Home.js
+++ MyMobileApp/src/components/Home.js
@@ -2,3 +2,14 @@ import { useNavigation } from '@react-navigation/native';
 import { styleSheet, View, ScrollView } from 'react-native';
 import { PaperProvider, Text, Divider, Button } from 'react-native-paper';
 
+export default function Home(){
+    return(
+        <PaperProvider>
+            <ScrollView contentContainerStyle={styles.ScrollView}>
+                <View>
+                    
+                </View>
+            </ScrollView>
+        </PaperProvider>
+    )
+}
