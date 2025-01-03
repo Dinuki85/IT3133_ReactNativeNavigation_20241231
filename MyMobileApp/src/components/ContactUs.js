8c5201a6dadfb20249fc57214fccc003f26ed1b9
Dinuki Prarthana <139613301+Dinuki85@users.noreply.github.com>
2025-01-03 07:30:10 +0000

    feat: initialize ContactUs component and import dependencies

---
+++ MyMobileApp/src/components/ContactUs.js
@@ -0,0 +1,5 @@
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { Platform } from 'react-native';
+import { StyleSheet, View, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { PaperProvider, Text, Divider, TextInput, Button } from 'react-native-paper';
